e1a2f755a3276ad1e114487ae1116be9975014e3
Maya Imhoff <mayelyimhoff@gmail.com>
2020-08-12 13:12:59 +0200

    seeds added, restaurant and reviews created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+5.times do
+   restaurant = Restaurant.create(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::Number.leading_zero_number(digits: 10)
+    )
+
+  15.times do
+    review = Review.create(
+    content: Faker::Lorem.paragraph,
+    rating: rand(0..5)
+    )
+    review.restaurant = restaurant
+    review.save
+  end
+  restaurant.save
+end
+
+#   post.user = user
+#     post.save
+#   end
+#   user.save
+# end
+
+# puts "Cleaning database..."
+# Restaurant.destroy_all
+
+# puts "Creating some restaurants..."
+# 10.times do
+#   restaurant = Restaurant.create(
+#     name: Faker::Company.name,
+#     address: Faker::Address.street_address,
+#     category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+#     rating: rand(0..5)
+#     )
+
+# 50.times do
+#   review = Review.create(
+#     text: Faker::Lorem.paragraph
+#     rating: rand(0..5)
+#   )
+# end
+# end
+# puts "Created #{Restaurant.count} restaurants and #{Review.count} ratings"
+# ["chinese", "italian", "japanese", "french", "belgian"]
+
+ # t.string "name"
+ #    t.string "address"
+ #    t.string "phone_number"
+ #    t.datetime "created_at", precision: 6, null: false
+ #    t.datetime "updated_at", precision: 6, null: false
+ #    t.string "category"
